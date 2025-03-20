7a9316ed641c0d1928cf8a48ab8b3b0bdb2215ce
Abhay <126308813+abhaysaqi@users.noreply.github.com>
2025-03-20 15:49:29 +0530

    Update constants.dart

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,6 +1,5 @@
 
 class Constants {
-  static final String youtubeApiKey = "AIzaSyAdOu0N4HvgUz4Gp8nNMfSGjmI8iPDWCg4";
-  // static const String youtubeApiKey = dotenv.env['YOUTUBE_API_KEY'];
+  static const String youtubeApiKey = dotenv.env['YOUTUBE_API_KEY'];
   static const int perPage = 20;
 }
